ff8e0bea4a99f4d949de09e4252e26cd8ff7bf7e
Glitch (quixotic-twisty-direction) <none>
2022-01-08 23:04:24 +0000

    🚴🐚 Checkpoint ./config.js:31538309/1448

--- config.js
+++ config.js
@@ -1,20 +1,30 @@
 module.exports = {
-        px: '-',
-        playing: 'Lofty Music v2',
+  px: "-",
+  playing: "Lofty Music v2",
 
-    opt: {
-        DJ: {
-            enabled: false, //EĞER SADECE DJLER KULLANA BİLSİN İSTİYOR İSENİZ false yazanı true yapın.
-            roleName: 'DJ', //DJ ROLÜNÜN İSMİ NE OLACAK İSE YAZIN SUNUCUNUZDA O ROLDEKİLER KULLANA BİLİR
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //DOKUNMA
-        },
-        maxVol: 200, //maximum ses seviyesi kaç olacak belirte bilirsiniz.
-        loopMessage: false, //elleme
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio', //elleme
-                highWaterMark: 1 << 25 //elleme
-            }
-        }
-    }
+  opt: {
+    DJ: {
+      enabled: false, //EĞER SADECE DJLER KULLANA BİLSİN İSTİYOR İSENİZ false yazanı true yapın.
+      roleName: "DJ", //DJ ROLÜNÜN İSMİ NE OLACAK İSE YAZIN SUNUCUNUZDA O ROLDEKİLER KULLANA BİLİR
+      commands: [
+        "back",
+        "clear",
+        "filter",
+        "loop",
+        "pause",
+        "resume",
+        "skip",
+        "stop",
+        "volume",
+      ], //DOKUNMA
+    },
+    maxVol: 200, //maximum ses seviyesi kaç olacak belirte bilirsiniz.
+    loopMessage: false, //elleme
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio", //elleme
+        highWaterMark: 1 << 25, //elleme
+      },
+    },
+  },
 };
